372e17b908884d2fc376e84ddf5a8c5e03ac9bc7
HuguitoU <hugou770@gmail.com>
2024-06-14 11:56:18 -0600

    admin.py

--- prueba/registros/admin.py
+++ prueba/registros/admin.py
@@ -4,6 +4,10 @@ from .models import Alumnos
 # Register your models here.
 class AdministrarModelo(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
+    list_display = ('matricula', 'nombre', 'carrera', 'turno') #Visualizacion
+    search_fields = ('matricula', 'nombre', 'carrera', 'turno') #Barra de 
+    date_hierarchy = 'created'
+    list_filter = ('carrera', 'turno')
 
 admin.site.register(Alumnos, AdministrarModelo)
 
